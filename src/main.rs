68ede12eaa630af36a2b2e64b161a16f20a6a0d8
Kiearn Williams <kieranwilliams@vindico.net>
2025-01-16 23:01:44 +0000

    .

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+mod formatted_print;
+mod formatted_print_debug;
+mod formatted_print_display;
+mod formatted_print_formatting;
+mod primitives_literals_and_operators;
+mod primitives_tuples;
+mod primitives_arrays_and_slices;
+mod custom_types_structures;
+mod custom_types_enums;
+mod custom_types_enums_use;
+mod custom_types_enums_ctype;
+mod custom_types_enums_testcase_linked_lists;
+
+fn main() {
+    // formatted_print::run();
+    // formatted_print_debug::run();
+    // formatted_print_display::run();
+    // formatted_print_formatting::run();
+    // primitives_literals_and_operators::run();
+    // primitives_tuples::run();
+    // primitives_arrays_and_slices::run();
+    // custom_types_structures::run();
+    custom_types_enums_ctype::run();
+}
